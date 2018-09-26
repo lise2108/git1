964d4a351699cd18253a012698316eda98e47ffb
Lise-Lu Yin Aaen <lise2108@edu.eal.dk>
2018-09-26 09:50:23 +0200

    ændring pc1

--- git1/Program.cs
+++ git1/Program.cs
@@ -13,7 +13,7 @@ namespace git1
             // The code provided will print ‘Hello World’ to the console.
             // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
             Console.WriteLine("Hello git1 ændret på pc2");
-            Console.WriteLine("ny ændring");
+            Console.WriteLine("ny ændring på pc1");
             Console.ReadKey();
 
             // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
